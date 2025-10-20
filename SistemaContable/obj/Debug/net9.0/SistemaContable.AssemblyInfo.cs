3f51fb8994fdfebb095e205ca071c073b6cca7c2
Jordy-beep <jordyjoaquin07@gmail.com>
2025-10-20 15:21:27 -0600

    Agregados los modulos

--- SistemaContable/obj/Debug/net9.0/SistemaContable.AssemblyInfo.cs
+++ SistemaContable/obj/Debug/net9.0/SistemaContable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaContable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf8dcb46806f198a1cfcf7b595e155e657ec397")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaContable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaContable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
